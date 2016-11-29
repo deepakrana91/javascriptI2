9bc865a7ac46e85c691fceab85fd59c06081168f
deepakrana91 <deepakrana91@live.com>
2016-11-29 12:13:33 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,10 @@
+ 
+ var obj1 = {name:"deepak", age:25 , DOB:"20/11/1991",placeofbirth:"roorkee"};
+  object();
+ function object ()
+        {
+            
+            console.log(obj1.name);
+            console.log(obj1.age);
+            console.log(obj1.DOB,obj1.placeofbirth);
+        }
